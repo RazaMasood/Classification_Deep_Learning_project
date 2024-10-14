296e22472f5e2f07bf17bda8328920b294232722
RazaMasood <razamasood954@gmail.com>
2024-10-14 13:58:26 +0530

    evaluation and mlflow added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kidney_Disease_classifier import logger
 from kidney_Disease_classifier.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from kidney_Disease_classifier.pipeline.prepare_base_model import PrepareBaseModelTrainingPipeline
 from kidney_Disease_classifier.pipeline.model_training import ModelTrainingPipeline
-
+from kidney_Disease_classifier.pipeline.model_evaluation_mlflow import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -32,6 +32,17 @@ try:
       logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
       model_trainer = ModelTrainingPipeline()
       model_trainer.main()
+      logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Evaluation stage"
+try:
+      logger.info(f"*******************")
+      logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+      model_evaluation = EvaluationPipeline()
+      model_evaluation.main()
       logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
